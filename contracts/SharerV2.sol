9029b1fba28f2c05cc62fd7f5d0a9ff10f4e16eb
Sam Priestley <sfpriestley@hotmail.co.uk>
2021-01-29 11:46:34 +0000

    sharer update

---
+++ contracts/SharerV2.sol
@@ -0,0 +1,75 @@
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+contract SharerV2 {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+    
+    
+    event contributorAdded(address _con, uint256 _shares, uint256 _total);
+    event contributorDeleted(address _con, uint256 _total);
+    struct Contributor {
+        uint256 numOfShares;
+        address owner;
+    }
+    mapping(address => Contributor[]) public shares;
+    address public strategist_ms;
+
+
+    constructor() public {
+        strategist_ms = msg.sender;
+    }
+
+    function changeStratMs(address _ms) external {
+        require(msg.sender == strategist_ms);
+        strategist_ms = _ms;
+    }
+
+    function viewContributors(address lpToken) public view returns (Contributor[] memory){
+       return shares[lpToken];
+    }
+
+
+    function setContributors(address lpToken, uint256[] calldata  contributorsS, address[] calldata contributorA) public {
+        require(contributorA.length == contributorsS.length, "length not the same");
+
+        require(shares[lpToken].length == 0 || msg.sender == strategist_ms, "Only Strat MS can overwrite");
+
+        delete shares[lpToken];
+        uint256 totalShares = 0;
+
+        for(uint256 i = 0; i < contributorsS.length; i++ ){
+            totalShares = totalShares.add(contributorsS[i]);
+            
+            shares[lpToken].push(Contributor(contributorsS[i], contributorA[i]));
+        }
+        require(totalShares <= 1000, "share total more than 100%");
+
+    }
+
+
+    function distribute(address lpToken) public{
+        IERC20 reward = IERC20(lpToken);
+        Contributor[] memory contributorsT = shares[lpToken];
+
+        uint256 totalRewards = reward.balanceOf(address(this));
+        uint256 remainingRewards = totalRewards;
+        if(totalRewards <= 1000){
+           return; 
+        }
+            
+        for(uint256 i = 0; i < contributorsT.length; i++ ){
+                address cont = contributorsT[i].owner;
+                uint256 share = totalRewards.mul(contributorsT[i].numOfShares).div(1000);
+                reward.safeTransfer(cont, share);
+                remainingRewards -= share;
+        }
+        reward.safeTransfer(strategist_ms, remainingRewards);
+        
+    }
+}
--- tests/test_share_distribution.py
+++ tests/test_share_distribution.py
@@ -3,22 +3,45 @@ from brownie import Wei, reverts
 import random
 import brownie
 
-def test_share_distro(chain, interface, accounts, Contract, Sharer):
+def test_share_distro(chain, interface, accounts, Contract, SharerV2):
     sms = '0x16388463d60FFE0661Cf7F1f31a7D658aC790ff7'
     gspoosi = '0x0C703068a36D7D2199B28B67422B33Ce627B87F6'
     dmi = '0x0D77b4C6916BA6dF46dCbc4809C204dA9089FEE2'
 
     samdev = accounts.at('0xC3D6880fD95E06C816cB030fAc45b3ffe3651Cb0', force=True)
+    rando = accounts[0]
+
+    mat = '0xd9c68eb096db712FFE15ede78B3D020903F8aa30'
+    ryan = '0xf1a692F2B7Da63670bf00c7376f630234Ea1bC2F'
     
-    sharer = samdev.deploy(Sharer)
+    sharer = samdev.deploy(SharerV2)
 
-    contributorsEx = [250,250,250]
+    contributorsEx = [250, 250, 250]
 
-    contributorA = [sms, gspoosi, dmi]
-
-    sharer.setContributors(contributorsEx,contributorA, {'from': samdev} )
-
+    contributorA = [mat, ryan, sms]
     yshare = interface.ERC20('0x19d3364a399d251e894ac732651be8b0e4e85001')
+
+    sharer.setContributors(yshare, contributorsEx,contributorA, {'from': rando} )
+
+    ##overwrite
+    with brownie.reverts("Only Strat MS can overwrite"):
+        sharer.setContributors(yshare, contributorsEx,contributorA, {'from': rando} )
+
+    sharer.setContributors(yshare, contributorsEx,contributorA, {'from': samdev} )
+
+    ##too many
+    contributorsEx = [100, 100, 100, 100]
+    with brownie.reverts("length not the same"):
+        sharer.setContributors(yshare, contributorsEx,contributorA, {'from': samdev} )
+
+    ##over 100%
+    contributorsEx = [500, 500, 500]
+    with brownie.reverts("share total more than 100%"):
+        sharer.setContributors(yshare, contributorsEx,contributorA, {'from': samdev} )
+
+    print(sharer.viewContributors(yshare))
+    print(sharer.viewContributors(dmi))
+
 
     yshare.transfer(sharer, yshare.balanceOf(samdev), {'from': samdev})
     print("Sam bal: ", yshare.balanceOf(samdev))
@@ -27,6 +50,6 @@ def test_share_distro(chain, interface, accounts, Contract, Sharer):
 
     sharer.distribute(yshare, {'from': samdev})
     print("Sam bal after dis: ", yshare.balanceOf(samdev)/1e18)
-    print("gspoosi bal: ", yshare.balanceOf(gspoosi)/1e18)
-    assert yshare.balanceOf(gspoosi) == yshare.balanceOf(dmi)
-    assert yshare.balanceOf(gspoosi) == yshare.balanceOf(sms)
+    print("Mat bal: ", yshare.balanceOf(mat)/1e18)
+    assert yshare.balanceOf(mat) == yshare.balanceOf(ryan)
+    assert yshare.balanceOf(samdev) >= yshare.balanceOf(ryan)
